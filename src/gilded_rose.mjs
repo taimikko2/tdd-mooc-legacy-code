9a838f9037a3e327267a60c70ca291d115e6442e
Mikko Saarinen <el4302@gmail.com>
2023-02-03 15:55:04 +0200

    Sulfuras quality += 1

--- src/gilded_rose.mjs
+++ src/gilded_rose.mjs
@@ -44,7 +44,7 @@ export class Shop {
           if (this.items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
             if (this.items[i].quality > 0) {
               if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
-                this.items[i].quality = this.items[i].quality - 1;
+                this.items[i].quality -= 1;
               }
             }
           } else {
