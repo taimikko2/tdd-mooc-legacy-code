cf2be0309f59382a8fa0a9fa23a1b0db5021e645
Mikko Saarinen <el4302@gmail.com>
2023-02-03 13:27:19 +0200

    npm run coverage 100%

--- test/gilded_rose.test.mjs
+++ test/gilded_rose.test.mjs
@@ -2,9 +2,22 @@ import { expect } from "chai";
 import { Item, Shop } from "../src/gilded_rose.mjs";
 
 describe("Gilded Rose", () => {
+  it("should Aged Brie", () => {
+    const gildedRose = new Shop([new Item("Aged Brie", 0, 0)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].name).to.equal("Aged Brie");
+  });
+
+  it("should Backstage passes", () => {
+    const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 0, 0)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].name).to.equal("Backstage passes to a TAFKAL80ETC concert");
+  });
+
   it("should foo", () => {
-    const gildedRose = new Shop([new Item("foo", 0, 0)]);
+    const gildedRose = new Shop([new Item("foo", 0, 3)]);
     const items = gildedRose.updateQuality();
     expect(items[0].name).to.equal("foo");
   });
+  
 });
