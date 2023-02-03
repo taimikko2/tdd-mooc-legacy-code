c5645759be6007cf7f8a386c5ec64353dc1b3e60
Mikko Saarinen <el4302@gmail.com>
2023-02-03 14:00:19 +0200

    npm run mutation 40 / 65

--- test/gilded_rose.test.mjs
+++ test/gilded_rose.test.mjs
@@ -18,12 +18,20 @@ describe("Gilded Rose", () => {
     const gildedRose = new Shop([new Item("foo", 0, 3)]);
     const items = gildedRose.updateQuality();
     expect(items[0].name).to.equal("foo");
+    expect(items[0].quality).to.equal(1); 
   });
   
-  it("should []]", () => {
+  it("should []", () => {
     const gildedRose = new Shop();
     const items = gildedRose.updateQuality();
     expect(items.length).to.equal(0);
+  });
+  
+  it("should handle several items", () => {
+    let originalItems = [new Item("Aged Brie", 2, 50), new Item("Backstage passes to a TAFKAL80ETC concert", 3, 3), new Item("foo", 4, 4)];
+    const gildedRose = new Shop(originalItems);
+    const items = gildedRose.updateQuality();
+    expect(items.length).to.equal(3);
   });
   
 });
