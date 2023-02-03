2125c96140c5efb66b03e84ff68264b1ba891c91
Mikko Saarinen <el4302@gmail.com>
2023-02-03 15:56:31 +0200

    quality += 1

--- src/gilded_rose.mjs
+++ src/gilded_rose.mjs
@@ -25,7 +25,7 @@ export class Shop {
           if (this.items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
             if (this.items[i].sellIn < 11) {
               if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
+                this.items[i].quality += 1;
               }
             }
             if (this.items[i].sellIn < 6) {
