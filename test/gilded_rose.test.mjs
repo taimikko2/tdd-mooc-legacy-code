87887e509022130eb0f18e61d9686a00e780a6fa
Mikko Saarinen <el4302@gmail.com>
2023-02-03 16:58:18 +0200

    test for Conjured items

--- test/gilded_rose.test.mjs
+++ test/gilded_rose.test.mjs
@@ -84,4 +84,14 @@ describe("Gilded Rose", () => {
     const items = gildedRose.updateQuality();
     expect(items.length).to.equal(3);
   });
+
+  it ("should hanle Conjured items", () => {
+    const gildedRose = new Shop([new Item("Conjured items", 0, 5), new Item("Conjured items", 5, 10)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].name).to.equal("Conjured items");
+    //expect(items[0].sellIn).to.equal(-1);
+    //expect(items[0].quality).to.equal(1);
+    //expect(items[1].sellIn).to.equal(4);
+    //expect(items[1].quality).to.equal(8);
+  });
 });
