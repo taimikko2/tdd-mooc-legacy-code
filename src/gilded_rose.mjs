913cc84c038731c7924fc90f5421050e5c536a72
Mikko Saarinen <el4302@gmail.com>
2023-02-03 16:15:43 +0200

    sullfuras removed

--- src/gilded_rose.mjs
+++ src/gilded_rose.mjs
@@ -17,10 +17,8 @@ export class Shop {
         continue;
       }
       if (this.items[i].name != "Aged Brie" && this.items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-        if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
-          if (this.items[i].quality > 0) {
-            this.items[i].quality -= 1;
-          }
+        if (this.items[i].quality > 0) {
+          this.items[i].quality -= 1;
         }
       } else {
         if (this.items[i].quality < 50) {
@@ -39,9 +37,8 @@ export class Shop {
           }
         }
       }
-      if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
-        this.items[i].sellIn -= 1;
-      }
+      this.items[i].sellIn -= 1;
+
       if (this.items[i].sellIn < 0) {
         if (this.items[i].name == "Aged Brie") {
           if (this.items[i].quality < 50) {
@@ -50,10 +47,8 @@ export class Shop {
         } else if (this.items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
           this.items[i].quality = 0;
         } else {
-          if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
-            if (this.items[i].quality > 0) {
-              this.items[i].quality -= 1;
-            }
+          if (this.items[i].quality > 0) {
+            this.items[i].quality -= 1;
           }
         }
       }
