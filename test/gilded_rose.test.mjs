7fec9fe7c4e4730188cdfb94156ca4e6c5c19efb
Mikko Saarinen <el4302@gmail.com>
2023-02-03 15:28:12 +0200

    92 / 13

--- test/gilded_rose.test.mjs
+++ test/gilded_rose.test.mjs
@@ -6,36 +6,42 @@ describe("Gilded Rose", () => {
     const gildedRose = new Shop([new Item("Aged Brie", 0, 0), new Item("Aged Brie", 0, 50)]);
     const items = gildedRose.updateQuality();
     expect(items[0].name).to.equal("Aged Brie");
-    expect(items[0].sellIn).to.equal(-1); 
-    expect(items[0].quality).to.equal(2); 
-    expect(items[1].sellIn).to.equal(-1); 
-    expect(items[1].quality).to.equal(50); 
+    expect(items[0].sellIn).to.equal(-1);
+    expect(items[0].quality).to.equal(2);
+    expect(items[1].sellIn).to.equal(-1);
+    expect(items[1].quality).to.equal(50);
   });
 
   it("should Backstage passes", () => {
-    const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 0, 0), new Item("Backstage passes to a TAFKAL80ETC concert", 50, 50)]);
+    const gildedRose = new Shop([
+      new Item("Backstage passes to a TAFKAL80ETC concert", 0, 0),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 50, 50),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 6, 6),
+    ]);
     const items = gildedRose.updateQuality();
     expect(items[0].name).to.equal("Backstage passes to a TAFKAL80ETC concert");
-    expect(items[0].sellIn).to.equal(-1); 
-    expect(items[0].quality).to.equal(0); 
-    expect(items[1].sellIn).to.equal(49); 
-    expect(items[1].quality).to.equal(50); 
+    expect(items[0].sellIn).to.equal(-1);
+    expect(items[0].quality).to.equal(0);
+    expect(items[1].sellIn).to.equal(49);
+    expect(items[1].quality).to.equal(50);
+    expect(items[2].sellIn).to.equal(5);
+    expect(items[2].quality).to.equal(8);
   });
 
   it("should foo", () => {
     const gildedRose = new Shop([new Item("foo", 0, 3), new Item("foo", 60, 60)]);
     const items = gildedRose.updateQuality();
     expect(items[0].name).to.equal("foo");
-    expect(items[0].quality).to.equal(1); 
-    expect(items[1].sellIn).to.equal(59); 
-    expect(items[1].quality).to.equal(59); 
+    expect(items[0].quality).to.equal(1);
+    expect(items[1].sellIn).to.equal(59);
+    expect(items[1].quality).to.equal(59);
   });
 
   it("should Sulfuras, Hand of Ragnaros", () => {
     const gildedRose = new Shop([new Item("Sulfuras, Hand of Ragnaros", 3, 3)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].sellIn).to.equal(3); 
-    expect(items[0].quality).to.equal(3); 
+    expect(items[0].sellIn).to.equal(3);
+    expect(items[0].quality).to.equal(3);
   });
 
   it("should []", () => {
@@ -43,23 +49,25 @@ describe("Gilded Rose", () => {
     const items = gildedRose.updateQuality();
     expect(items.length).to.equal(0);
   });
-  
-  it("should new Item(\"\")", () => {
+
+  it('should new Item("")', () => {
     const gildedRose = new Shop([new Item("", 0, 0), new Item("", 1, 50), new Item("", 0, 50)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].quality).to.equal(0); 
-    expect(items[1].sellIn).to.equal(0); 
-    expect(items[1].quality).to.equal(49); 
-    expect(items[2].sellIn).to.equal(-1); 
-    expect(items[2].quality).to.equal(48); 
+    expect(items[0].quality).to.equal(0);
+    expect(items[1].sellIn).to.equal(0);
+    expect(items[1].quality).to.equal(49);
+    expect(items[2].sellIn).to.equal(-1);
+    expect(items[2].quality).to.equal(48);
   });
 
-
   it("should handle several items", () => {
-    let originalItems = [new Item("Aged Brie", 2, 50), new Item("Backstage passes to a TAFKAL80ETC concert", 3, 3), new Item("foo", 4, 4)];
+    let originalItems = [
+      new Item("Aged Brie", 2, 50),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 3, 3),
+      new Item("foo", 4, 4),
+    ];
     const gildedRose = new Shop(originalItems);
     const items = gildedRose.updateQuality();
     expect(items.length).to.equal(3);
   });
-  
 });
