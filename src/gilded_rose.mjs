7e30232f0cc01f6499e70992e2b56e0ac5340bc2
Mikko Saarinen <el4302@gmail.com>
2023-02-03 16:27:09 +0200

    addQuality()

--- src/gilded_rose.mjs
+++ src/gilded_rose.mjs
@@ -11,27 +11,27 @@ export class Shop {
     this.items = items;
   }
 
+  addQuality(i) {
+    if (this.items[i].quality < 50) {
+      this.items[i].quality += 1;
+    }
+  }
+
   updateQuality() {
     for (var i = 0; i < this.items.length; i++) {
       if (this.items[i].name == "Sulfuras, Hand of Ragnaros") {
         continue;
       }
       if (this.items[i].name == "Aged Brie") {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality += 1;
-        }
+        this.addQuality(i);
       } else if (this.items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
         if (this.items[i].quality < 50) {
           this.items[i].quality += 1;
           if (this.items[i].sellIn < 11) {
-            if (this.items[i].quality < 50) {
-              this.items[i].quality += 1;
-            }
+            this.addQuality(i);
           }
           if (this.items[i].sellIn < 6) {
-            if (this.items[i].quality < 50) {
-              this.items[i].quality += 1;
-            }
+            this.addQuality(i);
           }
         }
       } else {
@@ -44,9 +44,7 @@ export class Shop {
 
       if (this.items[i].sellIn < 0) {
         if (this.items[i].name == "Aged Brie") {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality += 1;
-          }
+          this.addQuality(i);
         } else if (this.items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
           this.items[i].quality = 0;
         } else {
