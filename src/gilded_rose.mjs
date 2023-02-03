35904d6f959bbc12c716e4398755d7c066547737
Mikko Saarinen <el4302@gmail.com>
2023-02-03 16:45:58 +0200

    backstage quality 0

--- src/gilded_rose.mjs
+++ src/gilded_rose.mjs
@@ -35,15 +35,16 @@ export class Shop {
           this.addQuality(i);
         }
       } else if (this.items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
-        this.addQuality(i);
-        if (this.items[i].sellIn < 10) {
-          this.addQuality(i);
-        }
-        if (this.items[i].sellIn < 5) {
-          this.addQuality(i);
-        }
         if (this.items[i].sellIn < 0) {
           this.items[i].quality = 0;
+        } else {
+          this.addQuality(i);
+          if (this.items[i].sellIn < 10) {
+            this.addQuality(i);
+          }
+          if (this.items[i].sellIn < 5) {
+            this.addQuality(i);
+          }
         }
       } else {
         this.lessQuality(i);
