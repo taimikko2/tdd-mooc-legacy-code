f145a08979c00a0c73dad14dab1d7a904d2c38a3
Mikko Saarinen <el4302@gmail.com>
2023-02-03 16:08:30 +0200

    new order

--- src/gilded_rose.mjs
+++ src/gilded_rose.mjs
@@ -45,15 +45,15 @@ export class Shop {
             this.items[i].quality += 1;
           }
         } else {
-          if (this.items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
+          if (this.items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
+            this.items[i].quality = 0;
+          } else {
             if (this.items[i].quality > 0) {
               if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
                 this.items[i].quality -= 1;
               }
             }
-          } else {
-            this.items[i].quality = 0;
-          }
+          } 
         }
       }
     }
