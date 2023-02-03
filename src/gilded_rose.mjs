0d8cc3066923571f3c3d67e0398468fc7214bc4c
Mikko Saarinen <el4302@gmail.com>
2023-02-03 16:02:56 +0200

    changed order  : "Sulfuras, Hand of Ragnaros"

--- src/gilded_rose.mjs
+++ src/gilded_rose.mjs
@@ -14,8 +14,8 @@ export class Shop {
   updateQuality() {
     for (var i = 0; i < this.items.length; i++) {
       if (this.items[i].name != "Aged Brie" && this.items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
+        if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
+          if (this.items[i].quality > 0) {
             this.items[i].quality -= 1;
           }
         }
