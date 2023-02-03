3717a966d94c1ffe0abdf2b1be650ac417556002
Mikko Saarinen <el4302@gmail.com>
2023-02-03 14:30:12 +0200

    53/52

--- test/gilded_rose.test.mjs
+++ test/gilded_rose.test.mjs
@@ -12,12 +12,15 @@ describe("Gilded Rose", () => {
     const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 0, 0)]);
     const items = gildedRose.updateQuality();
     expect(items[0].name).to.equal("Backstage passes to a TAFKAL80ETC concert");
+    expect(items[0].sellIn).to.equal(-1); 
+    expect(items[0].quality).to.equal(0); 
   });
 
   it("should foo", () => {
     const gildedRose = new Shop([new Item("foo", 0, 3)]);
     const items = gildedRose.updateQuality();
     expect(items[0].name).to.equal("foo");
+    //expect(items[0].sellIn).to.equal(-1); 
     expect(items[0].quality).to.equal(1); 
   });
 
